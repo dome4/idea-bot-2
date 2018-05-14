8ba4f2346df5d8193b73ee55b467b7aec4fd7bea
dome4 <dominic.seitz@gmx.de>
2018-05-14 14:46:00 +0200

    routes updated

--- src/routes.php
+++ src/routes.php
@@ -3,26 +3,25 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-// Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->group('/api', function () {
+    $this->group('/v1', function () {
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
 
-/**
- * create new idea
- */
-$app->post('/idea', function ($request, $response, $args) {
-    echo $request;
-});
+        /**
+         * create new idea
+         */
+        $this->post('/idea', function ($request, $response, $args) {
+            
+            return $response;
+        });
 
-/**
- * get a random idea
- */
-$app->get('/random-idea', function ($request, $response, $args) {
-    echo $request;
+        /**
+         * get a random idea
+         */
+        $this->get('/random-idea', function ($request, $response, $args) {
+            
+            return $response;
+        });
+    });
 });
